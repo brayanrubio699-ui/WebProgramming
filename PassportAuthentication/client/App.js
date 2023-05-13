d3b34fec444132d85993abb2eec08abedb6586ca
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2023-05-13 14:06:26 -0500

    Update App.js

--- PassportAuthentication/client/App.js
+++ PassportAuthentication/client/App.js
@@ -1,7 +1,7 @@
 import Navbar from './components/Navbar';
 import "./App.css";
-import Home from "./pages/Home";
-import Post from './pages/Post';
+//import Home from "./pages/Home";
+//import Post from './pages/Post';
 import Login from "./pages/Login"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
@@ -46,11 +46,9 @@ const App = () => {
     <div>
       <Navbar user={user}/>
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/login" 
-        element={user ? <Navigate to="/" /> : <Login/>}
-        />
-        <Route path="/post/:id" element={user ? <Post/> : <Navigate to="/login" />} />
+        //<Route path="/" element={<Home />}/>
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login/>}        />
+        //<Route path="/post/:id" element={user ? <Post/> : <Navigate to="/login" />} />
       </Routes>
     </div>
     </BrowserRouter>
