1ca5c5e86f602eaf41b6ecc50290a674bdc2119f
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2023-09-23 15:05:44 -0500

    Create footer.js

---
+++ 2023/ReactApp/Components/footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+export default function Footer(props)
+{
+    return(
+        <div>{props.content}</div>
+    );
+}
