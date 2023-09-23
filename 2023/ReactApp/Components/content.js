76c6f4abe66af8b18609976cbc2067bd14f8571b
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2023-09-23 15:06:13 -0500

    Create content.js

---
+++ 2023/ReactApp/Components/content.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+export default function Content(props)
+{
+    return(
+        <div>{props.content}</div>
+    );
+}
