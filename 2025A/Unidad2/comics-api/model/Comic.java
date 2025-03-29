3909417b95d82f35d3c73d68c8eb1a648be23632
dfloaizab <65038210+dfloaizab@users.noreply.github.com>
2025-03-29 14:46:28 -0500

    Create Comic.java

---
+++ 2025A/Unidad2/comics-api/model/Comic.java
@@ -0,0 +1,19 @@
+package com.example.comicsapi.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
+
+@Data
+@Document(collection = "comics")
+public class Comic {
+    @Id
+    private String id;
+    private String titulo;
+    private int anioPublicacion;
+    private String editorial;
+    private String sinopsis;
+    private List<String> generos;
+    private String autorId; // Referencia al autor
+}
